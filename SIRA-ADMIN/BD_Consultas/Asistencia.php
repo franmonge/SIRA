1f5bd9cf7a0005749f2305081d81db97e48edd31
Francisco Monge Zúñiga <franmonge06@gmail.com>
2019-05-01 23:33:20 -0600

    asistencia

--- SIRA-ADMIN/BD_Consultas/Asistencia.php
+++ SIRA-ADMIN/BD_Consultas/Asistencia.php
@@ -1,19 +1,22 @@
 <?php
 
-function deactivate($id){
-  require('Conexion.php');
+function ausente($id, $gNombre){
+  require('Conexion.php');  
   if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
   }else{
+    $idGrupo = consultaGrupo($gNombre, $conn);
+    #$query = "INSERT INTO uxg(id_Grupo, id_Usuario) VALUES ('$idGrupo', '$id')";
+    #$result = mysqli_query($conn, $query);
     $query = "UPDATE usuario SET estado='Inactivo' WHERE id='$id'";
     $result = mysqli_query($conn, $query);
     $conn->close();
-    header("Location: ..\adminMiembros.php"); /* Redirect browser */
+    header("Location: ..\adminAsistencia.php"); /* Redirect browser */
   exit();
 }
 }
 
-function activate($id){
+function presente($id){
   require('Conexion.php');
   if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
@@ -21,120 +24,239 @@ function activate($id){
     $query = "UPDATE usuario SET estado='Activo' WHERE id='$id'";
     $result = mysqli_query($conn, $query);
     $conn->close();
-    header("Location: ..\adminMiembros.php"); /* Redirect browser */
+    header("Location: ..\adminAsistencia.php"); /* Redirect browser */
   exit();
 }
 }
 
-function insertarEstudianteGrupo($id, $gNombre, $conn){
-  $idGrupo = consultaGrupo($gNombre, $conn);
-  $query = "INSERT INTO uxg(id_Grupo, id_Usuario) VALUES ('$idGrupo', '$id')";
-  $result = mysqli_query($conn, $query);
-}
-
-function accept($id, $gNombre){
+function consultaGrupo($gNombre){
   require('Conexion.php');
   if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
   }else{
-    $query = "UPDATE usuario SET estado='Activo' WHERE id='$id'";
-    $result = mysqli_query($conn, $query);
-    insertarEstudianteGrupo($id, $gNombre, $conn);
-    $conn->close();
-    header("Location: ..\adminSolicitudes.php"); /* Redirect browser */
-    exit();
-  }
-}
-
-function consultaGrupo($gNombre, $conn){
     $sql = "SELECT ID FROM grupo WHERE Nombre = '$gNombre'";
     $result = mysqli_query($conn, $sql);
     $result2 = $result->fetch_array(MYSQLI_NUM);
     $id_grupo = $result2[0];
     return $id_grupo;
   }
+}
 
-function reject($id){
+function crearEnsayo($idGrupo,$fecha){
   require('Conexion.php');
+  require('..\..\sesion.php');
   if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
   }else{
-    $query = "DELETE FROM usuario WHERE id='$id'";
-    $result = mysqli_query($conn, $query);
-    $conn->close();
-    header("Location: ..\adminSolicitudes.php"); /* Redirect browser */
-  exit();
-}
-}
-
-
-function add($id){
-  require('Conexion.php');
-  if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-  }else{
-    $query = "INSERT INTO administrador(id_Usuario) VALUES ('$id')";
-    $result = mysqli_query($conn, $query);
-    $conn->close();
-    header("Location: ..\adminAdministradores.php"); /* Redirect browser */
-  exit();
-}
+  $query = "INSERT INTO ensayo(idGrupo, fecha) VALUES ('$idGrupo','$fecha')";
+  $result = mysqli_query($conn, $query);
+  $conn->close();
+  $_SESSION["CargarEstudiantes"] = $idGrupo;
+  header("Location: ..\adminAsistencia.php"); /* Redirect browser */
+  }
 }
 
+if (!empty($_POST["ensayo"])) {
+    $idGrupo = filter_input(INPUT_POST, 'ensayo');
+    $fecha = date("Y/m/d");
+    crearEnsayo($idGrupo,$fecha);
+} 
 
-function remove($id){
-  require('Conexion.php');
-  if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-  }else{
-    $query = "DELETE FROM administrador WHERE id_Usuario='$id'";
-    $result = mysqli_query($conn, $query);
-    $conn->close();
-    header("Location: ..\adminAdministradores.php"); /* Redirect browser */
-  exit();
-}
-}
+if (!empty($_POST["IdAusente"])) {
+    $uId = filter_input(INPUT_POST, 'IdAusente');
+    $gNombre = $_POST['Grupos'];
+    echo $gNombre;
+    ausente($uId,$gNombre);
+} 
 
-if (!empty($_POST["deactivateId"])) {
-    $uId = filter_input(INPUT_POST, 'deactivateId');
-    deactivate($uId);
-} else {
-    echo "No post received";
-}
-
-if (!empty($_POST["activateId"])) {
-    $uId = filter_input(INPUT_POST, 'activateId');
-    activate($uId);
-} else {
-    echo "No post received";
-}
-
-if (!empty($_POST["acceptId"])) {
-    $uId = filter_input(INPUT_POST, 'acceptId');
+if (!empty($_POST["IdPresente"])) {
+    $uId = filter_input(INPUT_POST, 'IdPresente');    
     $gNombre = filter_input(INPUT_POST, 'Grupos');
-    accept($uId, $gNombre);
-} else {
-    echo "No post received";
+    presente($uId,$gNombre);
+} 
+
+
+function miembrosPresentes($idGrupo){
+  require('Conexion.php');
+  if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    $query = "SELECT U.id, U.nombre, U.apellidos, U.email FROM usuario as U INNER JOIN uxg as A WHERE U.estado='Activo' ";
+    $result = mysqli_query($conn, $query);
+    if($result->num_rows > 0){
+      $Codigo = "
+      <section class=\"content\">
+      <div class=\"row\">
+      <div class=\"col-xs-12\">
+      <div class=\"box\">
+      <div class=\"box-header\">
+      <h3 class=\"box-title\">Presentes</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class=\"box-body\">
+      <table id=\"table-Miembros-Presentes\" class=\"table table-bordered table-striped\">
+      <thead>
+      <tr>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Email</th>
+      <th>Ausentes</th>
+      </tr>
+      </thead>
+      <tbody>";
+
+      while($row = $result->fetch_assoc()){
+        $Codigo .= "<tr>";
+        $Codigo .= "<td>".$row["nombre"]."</td>";
+        $Codigo .= "<td>" .$row["apellidos"] . "</td>";
+        $Codigo .= "<td>" .$row["email"] . "</td>";
+        $Codigo .= "<td>" ."<form action=\"BD_Consultas\Asistencia.php\" method=\"post\">
+        <input type=\"hidden\" name=\"IdAusente\" value=\"".$row["id"]."\" >
+        <input type=\"submit\"  class=\"btn btn-block btn-danger btn-flat\" value=\"Ausente\">
+        </form></td>";
+        $Codigo .= "</tr>";
+      }
+      $Codigo .= "
+      </tbody>
+      <tfoot>
+      <tr>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Email</th>
+      <th>Ausentes</th>
+      </tr>
+      </tfoot>
+      </table>
+      </div>
+      <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+      </div>
+      <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      </section>
+      <!-- /.content -->";
+      echo $Codigo;
+    }else {
+      $Codigo = "<section class=\"content\">
+      <div class=\"row\">
+      <div class=\"col-xs-12\">
+      <div class=\"box\">
+      <div class=\"box-header\">
+      <h3 class=\"box-title\">Presentes</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class=\"box-body\">
+      <h2> No hay miembros presentes</h2>
+      </div>
+      <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+      </div>
+      <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      </section>
+      <!-- /.content -->";
+      echo $Codigo;
+    }
+  }
+  $conn->close();
 }
 
-if (!empty($_POST["rejectId"])) {
-    $uId = filter_input(INPUT_POST, 'rejectId');
-    reject($uId);
-} else {
-    echo "No post received";
+
+function miembrosAusentes($idGrupo){
+  require('BD_Consultas\Conexion.php');
+  if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    $query = "  SELECT id, nombre, apellidos, email FROM usuario WHERE estado='Inactivo'";
+    $result = mysqli_query($conn, $query);
+    if($result->num_rows > 0){
+      $Codigo = "
+      <!-- Main content -->
+      <section class=\"content\">
+      <div class=\"row\">
+      <div class=\"col-xs-12\">
+      <div class=\"box\">
+      <div class=\"box-header\">
+      <h3 class=\"box-title\">Ausentes</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class=\"box-body\">
+
+      <table id=\"table-Miembros-Ausentes\" class=\"table table-bordered table-striped\">
+      <thead>
+      <tr>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Email</th>
+      <th>Presentes</th>
+      </tr>
+      </thead>
+      <tbody>";
+
+      while($row = $result->fetch_assoc()){
+        $Codigo .= "<tr>";
+        $Codigo .= "<td>".$row["nombre"]."</td>";
+        $Codigo .= "<td>" .$row["apellidos"] . "</td>";
+        $Codigo .= "<td>" .$row["email"] . "</td>";
+        $Codigo .= "<td>" ."<form action=\"BD_Consultas\Asistencia.php\" method=\"post\">
+        <input type=\"hidden\" name=\"IdPresente\" value=\"".$row["id"]."\" >
+        <input type=\"submit\"  class=\"btn btn-block btn-primary btn-flat\" value=\"Presente\">
+        </form></td>";
+        $Codigo .= "</tr>";
+      }
+      $Codigo .= "
+
+      </tbody>
+      <tfoot>
+      <tr>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Email</th>
+      <th>Presentes</th>
+      </tr>
+      </tfoot>
+      </table>
+      </div>
+      <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+      </div>
+      <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      </section>
+      <!-- /.content -->";
+      echo $Codigo;
+    }else {
+      $Codigo = " <!-- Main content -->
+      <section class=\"content\">
+      <div class=\"row\">
+      <div class=\"col-xs-12\">
+      <div class=\"box\">
+      <div class=\"box-header\">
+      <h3 class=\"box-title\">Ausentes</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class=\"box-body\">
+      <h2> No hay miembros ausentes </h2>
+      </div>
+      <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+      </div>
+      <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      </section>
+      <!-- /.content -->";
+      echo $Codigo;
+    }
+  }
+  $conn->close();
 }
 
-if (!empty($_POST["addAdmin"])) {
-    $uId = filter_input(INPUT_POST, 'addAdmin');
-    add($uId);
-} else {
-    echo "No post received";
-}
-
-if (!empty($_POST["removeAdmin"])) {
-    $uId = filter_input(INPUT_POST, 'removeAdmin');
-    remove($uId);
-} else {
-    echo "No post received";
-}
- ?>
+?>

5c5e39d16bc7cb852791cb6239903d7ef29ba409
kafayng <kafay2612@gmail.com>
2019-05-09 18:11:43 -0600

    Commit: Filtros por Carrera y Grupos completado

--- SIRA-ADMIN/BD_Consultas/miembros.php
+++ SIRA-ADMIN/BD_Consultas/miembros.php
@@ -1,4 +1,98 @@
 <?php
+function tablaGrupos($filtro){
+  require('BD_Consultas\Conexion.php');
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    $query = "SELECT usuario.id, usuario.nombre, usuario.apellidos, usuario.email FROM usuario JOIN uxg ON uxg.id_Grupo = (SELECT id FROM grupo WHERE nombre = '$filtro') AND uxg.id_Usuario=usuario.id;";
+    $result = mysqli_query($conn, $query);
+    if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row['nombre']."</td><td>".$row['apellidos']."</td><td>".$row['email']."</td><td><form action=\"BD_Consultas\miembros.php\" method=\"post\"><input type=\"hidden\" name=\"deactivateId\" value=\"".$row['id']."\"><input type=\"submit\"  class=\"btn btn-block btn-danger btn-flat\" value=\"Desactivar\"></form></td></tr>";
+      }
+    }
+  }
+}
+
+function tablaCarreras($filtro){
+  require('BD_Consultas\Conexion.php');
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    $idCarrera = consultaCarrera($filtro);
+    $query = "SELECT id, nombre, apellidos, email FROM usuario WHERE estado = 'Activo' AND id_Carrera = '$idCarrera'";
+    $result = mysqli_query($conn, $query);
+    if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row['nombre']."</td><td>".$row['apellidos']."</td><td>".$row['email']."</td><td><form action=\"BD_Consultas\miembros.php\" method=\"post\"><input type=\"hidden\" name=\"deactivateId\" value=\"".$row['id']."\"><input type=\"submit\"  class=\"btn btn-block btn-danger btn-flat\" value=\"Desactivar\"></form></td></tr>";
+      }
+    }
+  }
+}
+
+function consultaCarrera($filtro){
+  require('BD_Consultas\Conexion.php');
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    $query = "SELECT ID FROM carrera WHERE Nombre = '$filtro'";
+    $result = mysqli_query($conn, $query);
+    $result2 = $result->fetch_array(MYSQLI_NUM);
+    $id_Carrera = $result2[0];
+    return $id_Carrera;
+  }
+}    
+
+ function tablaTodos(){
+  require('BD_Consultas\Conexion.php');
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+      $query = "SELECT id, nombre, apellidos, email FROM usuario WHERE estado='Activo'";
+      $result = mysqli_query($conn, $query);
+      if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+          echo "<tr><td>".$row['nombre']."</td><td>".$row['apellidos']."</td><td>".$row['email']."</td><td><form action=\"BD_Consultas\miembros.php\" method=\"post\"><input type=\"hidden\" name=\"deactivateId\" value=\"".$row['id']."\"><input type=\"submit\"  class=\"btn btn-block btn-danger btn-flat\" value=\"Desactivar\"></form></td></tr>";
+      }
+    }
+  }
+}
+
+function dropdownCarreras(){
+    require('..\BD_Consultas\Conexion.php');
+    if ($conn->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+    }else{
+      $sql = "SELECT Nombre FROM carrera";
+      $result = mysqli_query($conn, $sql);
+      if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+          echo "<option value=\"".$row['Nombre']."\">".$row['Nombre']."</option>";
+        }
+      }else{
+        echo "<option>No hay carreras registradas</option>";
+      }
+    $conn->close();
+    }
+  }
+
+function dropdownGrupos(){
+  require('..\BD_Consultas\Conexion.php');
+    if ($conn->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+    }else{
+      $sql = "SELECT Nombre FROM grupo";
+      $result = mysqli_query($conn, $sql);
+      if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+          echo "<option>" .$row['Nombre']."</option>";
+        }
+      }else{
+          echo "<option>No hay grupos registrados</option>";
+      }
+      $conn->close();
+    }
+}
 
 function deactivate($id){
   require('Conexion.php');
@@ -9,8 +103,7 @@ function deactivate($id){
     $result = mysqli_query($conn, $query);
     $conn->close();
     header("Location: ..\adminMiembros.php"); /* Redirect browser */
-  exit();
-}
+  }
 }
 
 function activate($id){
@@ -22,8 +115,7 @@ function activate($id){
     $result = mysqli_query($conn, $query);
     $conn->close();
     header("Location: ..\adminMiembros.php"); /* Redirect browser */
-  exit();
-}
+  }
 }
 
 function insertarEstudianteGrupo($id, $gNombre, $conn){
@@ -95,46 +187,127 @@ function remove($id){
 }
 }
 
+function miembrosInactivos(){
+require('BD_Consultas\Conexion.php');
+if ($conn->connect_error){
+  die("Connection failed: " . $conn->connect_error);
+}else{
+  $query = "  SELECT id, nombre, apellidos, email FROM usuario WHERE estado='Inactivo'";
+  $result = mysqli_query($conn, $query);
+  if($result->num_rows > 0){
+    $Codigo = "
+     <!-- Main content -->
+     <section class=\"content\">
+       <div class=\"row\">
+         <div class=\"col-xs-12\">
+           <div class=\"box\">
+             <div class=\"box-header\">
+               <h3 class=\"box-title\">Inactivos</h3>
+             </div>
+             <!-- /.box-header -->
+             <div class=\"box-body\">
+
+               <table id=\"table-Miembros\" class=\"table table-bordered table-striped\">
+                 <thead>
+                 <tr>
+                   <th>Nombre</th>
+                   <th>Apellidos</th>
+                   <th>Email</th>
+                   <th>Desactivar</th>
+                 </tr>
+                 </thead>
+                 <tbody>";
+
+    while($row = $result->fetch_assoc()){
+      $Codigo .= "<tr>";
+      $Codigo .= "<td>".$row["nombre"]."</td>";
+      $Codigo .= "<td>" .$row["apellidos"] . "</td>";
+      $Codigo .= "<td>" .$row["email"] . "</td>";
+      $Codigo .= "<td>" ."<form action=\"BD_Consultas\miembros.php\" method=\"post\">
+      <input type=\"hidden\" name=\"activateId\" value=\"".$row["id"]."\" >
+      <input type=\"submit\"  class=\"btn btn-block btn-primary btn-flat\" value=\"Activar\">
+      </form></td>";
+      $Codigo .= "</tr>";
+    }
+    $Codigo .= "
+
+    </tbody>
+      <tfoot>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellidos</th>
+          <th>Email</th>
+          <th>Desactivar</th>
+        </tr>
+      </tfoot>
+    </table>
+    </div>
+        <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+    </div>
+    <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </section>
+    <!-- /.content -->";
+    echo $Codigo;
+  }else {
+    $Codigo = " <!-- Main content -->
+     <section class=\"content\">
+       <div class=\"row\">
+         <div class=\"col-xs-12\">
+           <div class=\"box\">
+             <div class=\"box-header\">
+               <h3 class=\"box-title\">Inactivos</h3>
+             </div>
+             <!-- /.box-header -->
+             <div class=\"box-body\">
+             <h2> No hay miembros inactivos </h2>
+             </div>
+                 <!-- /.box-body -->
+            </div>
+             <!-- /.box -->
+          </div>
+             <!-- /.col -->
+        </div>
+             <!-- /.row -->
+      </section>
+             <!-- /.content -->";
+             echo $Codigo;
+  }
+}
+$conn->close();
+}
+
 if (!empty($_POST["deactivateId"])) {
     $uId = filter_input(INPUT_POST, 'deactivateId');
     deactivate($uId);
-} else {
-    echo "No post received";
 }
 
 if (!empty($_POST["activateId"])) {
     $uId = filter_input(INPUT_POST, 'activateId');
     activate($uId);
-} else {
-    echo "No post received";
 }
 
 if (!empty($_POST["acceptId"])) {
     $uId = filter_input(INPUT_POST, 'acceptId');
     $gNombre = filter_input(INPUT_POST, 'Grupos');
     accept($uId, $gNombre);
-} else {
-    echo "No post received";
 }
 
 if (!empty($_POST["rejectId"])) {
     $uId = filter_input(INPUT_POST, 'rejectId');
     reject($uId);
-} else {
-    echo "No post received";
 }
 
 if (!empty($_POST["addAdmin"])) {
     $uId = filter_input(INPUT_POST, 'addAdmin');
     add($uId);
-} else {
-    echo "No post received";
 }
 
 if (!empty($_POST["removeAdmin"])) {
     $uId = filter_input(INPUT_POST, 'removeAdmin');
     remove($uId);
-} else {
-    echo "No post received";
 }
  ?>
--- SIRA-ADMIN/adminMiembros.php
+++ SIRA-ADMIN/adminMiembros.php
@@ -1,236 +1,25 @@
-<?php
-
-function dropdownCarreras(){
-    require('Conexion.php');
-    if ($conn->connect_error){
-      die("Connection failed: " . $conn->connect_error);
-    }else{
-      $sql = "SELECT Nombre FROM carrera";
-      $result = mysqli_query($conn, $sql);
-      $resultado = "";
-      if($result->num_rows > 0){
-        while($row = $result->fetch_assoc()){
-          $resultado."<option>" .$row['Nombre']."</option>";
-        }
-      }else{
-        $resultado."<option>No hay carreras registradas</option>";
-      }
-      return $resultado;
-    $conn->close();
-    }
+ <?php
+if(isset($_POST['functionname']) && !empty($_POST["functionname"])){
+  include('BD_Consultas\miembros.php');
+  switch($_POST["functionname"]){
+  case 'llenaDropdown':
+    dropdownCarreras();
+    break;
+  case 'llenaDropdownGrupo':
+    dropdownGrupos();
+    break;
+  case 'todosTabla':
+    tablaTodos();
+    break;
+  case 'gruposTabla':
+    tablaGrupos($_POST['filtro']);
+    break;
+  case 'carrerasTabla':
+    tablaCarreras($_POST['filtro']);
+    break;
   }
-
-function miembrosActivos(){
-require('BD_Consultas\Conexion.php');
-if ($conn->connect_error){
-  die("Connection failed: " . $conn->connect_error);
-}else{
-  $query = "  SELECT id, nombre, apellidos, email FROM usuario WHERE estado='Activo'";
-  $result = mysqli_query($conn, $query);
-  if($result->num_rows > 0){
-    $Codigo = "
-     <!-- Main content -->
-     <section class=\"content\">
-       <div class=\"row\">
-         <div class=\"col-xs-12\">
-           <div class=\"box\">
-             <div class=\"box-header\">
-               <h3 class=\"box-title\">Activos</h3>
-             </div>
-              <span style=\"margin-left:10px; margin-right:10px\">Filtrar por:</span>
-                <select id=\"PrimerFiltro\">
-                  <option value=\"Carrera\">Carrera</option>
-                  <option value=\"Grupo\">Grupo</option>
-                </select>
-                <select id=\"SegundoFiltro\">
-                </select>
-                <script>
-                  var dropdown1 = document.getElementById(\"PrimerFiltro\");
-                  var dropdown2 = document.getElementById(\"SegundoFiltro\");
-                  onchange();
-                  dropdown1.onchange = onchange;
-
-                  function onchange(){
-                      var opcionDropdown2 = \"\";
-                      if(dropdown1.value == \"Grupo\"){
-                        var opcionDropdown2 = \"<option>Grupo</option>\";
-                      }
-                      if(dropdown1.value == \"Carrera\"){
-                        var opcionDropdown2 = '<?php echo dropdownCarreras()?>';
-                      }
-                      dropdown2.innerHTML = opcionDropdown2;
-                  }
-                </script>
-             <!-- /.box-header -->
-             <div class=\"box-body\">
-
-               <table id=\"table-Miembros\" class=\"table table-bordered table-striped\">
-                 <thead>
-                 <tr>
-                   <th>Nombre</th>
-                   <th>Apellidos</th>
-                   <th>Email</th>
-                   <th>Desactivar</th>
-                 </tr>
-                 </thead>
-                 <tbody>";
-
-    while($row = $result->fetch_assoc()){
-      $Codigo .= "<tr>";
-      $Codigo .= "<td>".$row["nombre"]."</td>";
-      $Codigo .= "<td>" .$row["apellidos"] . "</td>";
-      $Codigo .= "<td>" .$row["email"] . "</td>";
-      $Codigo .= "<td>" ."<form action=\"BD_Consultas\miembros.php\" method=\"post\">
-      <input type=\"hidden\" name=\"deactivateId\" value=\"".$row["id"]."\" >
-      <input type=\"submit\"  class=\"btn btn-block btn-danger btn-flat\" value=\"Desactivar\">
-      </form></td>";
-      $Codigo .= "</tr>";
-    }
-    $Codigo .= "
-    </tbody>
-      <tfoot>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellidos</th>
-          <th>Email</th>
-          <th>Desactivar</th>
-        </tr>
-      </tfoot>
-    </table>
-    </div>
-        <!-- /.box-body -->
-    </div>
-    <!-- /.box -->
-    </div>
-    <!-- /.col -->
-    </div>
-    <!-- /.row -->
-    </section>
-    <!-- /.content -->";
-    echo $Codigo;
-  }else {
-    $Codigo = " <!-- Main content -->
-     <section class=\"content\">
-       <div class=\"row\">
-         <div class=\"col-xs-12\">
-           <div class=\"box\">
-             <div class=\"box-header\">
-               <h3 class=\"box-title\">Activos</h3>
-             </div>
-             <!-- /.box-header -->
-             <div class=\"box-body\">
-             <h2> No hay miembros activos </h2>
-             </div>
-                 <!-- /.box-body -->
-            </div>
-             <!-- /.box -->
-          </div>
-             <!-- /.col -->
-        </div>
-             <!-- /.row -->
-      </section>
-             <!-- /.content -->";
-             echo $Codigo;
-  }
-}
-$conn->close();
-}
-
-function miembrosInactivos(){
-require('BD_Consultas\Conexion.php');
-if ($conn->connect_error){
-  die("Connection failed: " . $conn->connect_error);
-}else{
-  $query = "  SELECT id, nombre, apellidos, email FROM usuario WHERE estado='Inactivo'";
-  $result = mysqli_query($conn, $query);
-  if($result->num_rows > 0){
-    $Codigo = "
-     <!-- Main content -->
-     <section class=\"content\">
-       <div class=\"row\">
-         <div class=\"col-xs-12\">
-           <div class=\"box\">
-             <div class=\"box-header\">
-               <h3 class=\"box-title\">Inactivos</h3>
-             </div>
-             <!-- /.box-header -->
-             <div class=\"box-body\">
-
-               <table id=\"table-Miembros\" class=\"table table-bordered table-striped\">
-                 <thead>
-                 <tr>
-                   <th>Nombre</th>
-                   <th>Apellidos</th>
-                   <th>Email</th>
-                   <th>Desactivar</th>
-                 </tr>
-                 </thead>
-                 <tbody>";
-
-    while($row = $result->fetch_assoc()){
-      $Codigo .= "<tr>";
-      $Codigo .= "<td>".$row["nombre"]."</td>";
-      $Codigo .= "<td>" .$row["apellidos"] . "</td>";
-      $Codigo .= "<td>" .$row["email"] . "</td>";
-      $Codigo .= "<td>" ."<form action=\"BD_Consultas\miembros.php\" method=\"post\">
-      <input type=\"hidden\" name=\"activateId\" value=\"".$row["id"]."\" >
-      <input type=\"submit\"  class=\"btn btn-block btn-primary btn-flat\" value=\"Activar\">
-      </form></td>";
-      $Codigo .= "</tr>";
-    }
-    $Codigo .= "
-
-    </tbody>
-      <tfoot>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellidos</th>
-          <th>Email</th>
-          <th>Desactivar</th>
-        </tr>
-      </tfoot>
-    </table>
-    </div>
-        <!-- /.box-body -->
-    </div>
-    <!-- /.box -->
-    </div>
-    <!-- /.col -->
-    </div>
-    <!-- /.row -->
-    </section>
-    <!-- /.content -->";
-    echo $Codigo;
-  }else {
-    $Codigo = " <!-- Main content -->
-     <section class=\"content\">
-       <div class=\"row\">
-         <div class=\"col-xs-12\">
-           <div class=\"box\">
-             <div class=\"box-header\">
-               <h3 class=\"box-title\">Inactivos</h3>
-             </div>
-             <!-- /.box-header -->
-             <div class=\"box-body\">
-             <h2> No hay miembros inactivos </h2>
-             </div>
-                 <!-- /.box-body -->
-            </div>
-             <!-- /.box -->
-          </div>
-             <!-- /.col -->
-        </div>
-             <!-- /.row -->
-      </section>
-             <!-- /.content -->";
-             echo $Codigo;
-  }
-}
-$conn->close();
 }
 ?>
-
-
 
  <!DOCTYPE html>
  <html>
@@ -238,23 +27,61 @@ $conn->close();
    <meta charset="utf-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <title>SIRA | Miembros</title>
-   <!-- Tell the browser to be responsive to screen width -->
    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
    <?php include('headerLinks.php')?>
+   <?php include('BD_Consultas\miembros.php')?>
+   <script src="../js/jquery-3.3.1.min.js"></script>
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
     <?php include('adminNav.php')?>
-    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-      <!-- Main content -->
       <section class="content-header">
         <h1>Miembros</h1>
       </section>
-
-       <?php miembrosActivos();
-             miembrosInactivos();?>
+      <section class="content">
+       <div class="row">
+         <div class="col-xs-12">
+           <div class="box">
+             <div class="box-header">
+               <h3 class="box-title">Activos</h3>
+             </div>
+              <span style="margin-left:10px; margin-right:10px">Filtrar por:</span>
+                  <select id="PrimerFiltro" name="selectPrimerFiltro">
+                    <option value="Todos">Todos</option>
+                    <option value="Carrera">Carrera</option>
+                    <option value="Grupo">Grupo</option>
+                  </select>
+                  <select id="SegundoFiltro" name="selectSegundoFiltro">
+                  </select>
+               <div class="box-body">
+                 <table id="table-Miembros" class="table table-bordered table-striped">
+                   <thead>
+                   <tr>
+                     <th>Nombre</th>
+                     <th>Apellidos</th>
+                     <th>Email</th>
+                     <th>Desactivar</th>
+                   </tr>
+                   </thead>
+                   <tbody>
+                   </tbody>
+                   <tfoot>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>Apellidos</th>
+                      <th>Email</th>
+                      <th>Desactivar</th>
+                    </tr>
+                   </tfoot>
+                 </table>
+               </div>
+           </div>
+         </div>
+       </div>
+     </section>
+     <?php miembrosInactivos();?>
     </div>
   <!-- ./wrapper -->
 
@@ -289,5 +116,92 @@ $conn->close();
     })
   })
   </script>
+  <script>
+  var dropdown1 = document.getElementById("PrimerFiltro");
+  var dropdown2 = document.getElementById("SegundoFiltro");
+  var table = document.getElementById("table-Miembros");
+  var tableBody = document.getElementsByTagName("tbody")[0];
+  onchange1();
+  // onchange2();
+  dropdown1.onchange = onchange1;
+  dropdown2.onchange = onchange2;
+
+  function onchange2(){
+    var segundoFiltro = dropdown2.value;
+    if(dropdown1.value == "Carrera"){
+      jQuery.ajax({
+        type:"POST",
+        url:'adminMiembros.php',
+        data: {functionname:'carrerasTabla', filtro:segundoFiltro},
+        success: function(data){
+          data = data.split('<!DOCTYPE')[0];
+          var tbody = "<tr><td>No hay miembros</td></tr>";
+          if($.trim(data)){
+            var tbody = data;
+          }
+            tableBody.innerHTML = tbody;
+        }
+      });
+    }
+    if(dropdown1.value == "Grupo"){
+      jQuery.ajax({
+        type:"POST",
+        url:'adminMiembros.php',
+        data: {functionname:'gruposTabla', filtro:segundoFiltro},
+        success: function(data){
+          data = data.split('<!DOCTYPE')[0];
+          var tbody = "<tr><td>No hay miembros</td></tr>";
+          if($.trim(data)){
+            var tbody = data;
+          }
+            tableBody.innerHTML = tbody;
+        }
+      });
+    }
+  }
+
+  function onchange1(){
+    if(dropdown1.value == "Todos"){
+      jQuery.ajax({
+        type:"POST",
+        url:'adminMiembros.php',
+        data: {functionname:'todosTabla'},
+        success: function(data){
+          data = data.split('<!DOCTYPE')[0];
+          var tbody = data;
+          tableBody.innerHTML = tbody;
+          dropdown2.innerHTML = "<option>-</option>";
+        }
+      });
+    }
+    if(dropdown1.value == "Carrera"){
+      jQuery.ajax({
+        type:"POST",
+        url:'adminMiembros.php',
+        data: {functionname:'llenaDropdown'},
+        success: function(data){
+          var opcionDropdown2 = "";
+          data = data.split('<!DOCTYPE')[0];
+          var opcionDropdown2 = "<option>-</option>"+data;
+          dropdown2.innerHTML = opcionDropdown2;
+        }
+      });
+    }
+    if(dropdown1.value == "Grupo"){
+      var segundoFiltro = dropdown2.value;
+      jQuery.ajax({
+        type:"POST",
+        url:'adminMiembros.php',
+        data: {functionname:'llenaDropdownGrupo'},
+        success: function(data){
+          var opcionDropdown2 = "";
+          data = data.split('<!DOCTYPE')[0];
+          var opcionDropdown2 = "<option>-</option>"+data;
+          dropdown2.innerHTML = opcionDropdown2;
+        }
+      });
+    }
+  }
+</script>
  </body>
  </html>

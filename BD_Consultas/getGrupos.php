81399c36812553f235aeb526f8e2cf7c4ecb5656
kafayng <kafay2612@gmail.com>
2019-06-11 11:41:33 -0600

    Commit: arreglo de size de las fotos de grupos

--- BD_Consultas/getGrupos.php
+++ BD_Consultas/getGrupos.php
@@ -14,7 +14,7 @@
 									<h3 class=\"card-header indigo white-text text-uppercase text-center\">" .$row['Nombre']. "</h3>
 									<div class=\"card-body\">
 										<div class=\"media d-block d-md-flex mt-md-0 mt-4 mb-4\">
-											<img class=\"d-flex mb-md-0 mb-3 avatar-2 z-depth-1 mx-auto\" src=\"".$row['Imagen']."\"
+											<img class=\"d-flex mb-md-0 mb-3 avatar-2 z-depth-1 mx-auto\" style=\"max-height:250px; max-width:250px;\"src=\"".$row['Imagen']."\"
 												alt=\"No image\">
 											<div class=\"media-body ml-md-3 ml-0\">
 												<p align=\"justify\">" .$row['Descripcion']."</p>
